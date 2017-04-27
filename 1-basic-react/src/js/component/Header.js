2569157bd7b5145bb7189e3d83fa418e1f610b2d
Eguono <efeguono@gmail.com>
2017-04-27 19:29:56 +0100

    State vs Props, Binding Events, Interacting with User, Changing the data real time

--- 1-basic-react/src/js/component/Header.js
+++ 1-basic-react/src/js/component/Header.js
@@ -3,9 +3,17 @@ import React from "react";
 import Title from "./Header/Title";
 
 export default class Header extends React.Component{
+
+    handleBars(e){
+        const title = e.target.value;
+        this.props.changeTitle(title)
+    }
     render(){
         return (
-            <header><Title/></header>
+            <div>
+                <header><Title title={this.props.title}/></header>
+                <input onChange={this.handleBars.bind(this)} value={this.props.title}/>
+            </div>
         );
     };
 };
--- 1-basic-react/src/js/component/Layout.js
+++ 1-basic-react/src/js/component/Layout.js
@@ -4,12 +4,19 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = { title: "welcome" }
+    }
 
+    changeTitle(title) {
+        this.setState({title});
+    }
     render() {
         return (
             <div>
-                <h1><Header/></h1>
-                <h1><Footer/></h1>
+                <h1><Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} /></h1>
+                <h1><Footer /></h1>
             </div>
         );
     };
